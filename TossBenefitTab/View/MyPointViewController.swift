15d7f9007ce1b5fdf8c086363302eebec405cfbd
seokhyun <letushyun@gmail.com>
2023-01-11 03:14:37 +0900

    configuring UI in MyPointVC

--- TossBenefitTab/View/MyPointViewController.swift
+++ TossBenefitTab/View/MyPointViewController.swift
@@ -6,13 +6,86 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
-class MyPointViewController: UIViewController {
+class MyPointViewController: BaseViewController {
+  // MARK: - Properties
+  let stackView = UIStackView().then {
+    $0.axis = .vertical
+    $0.distribution = .fill
+    $0.spacing = 5
+  }
+  
+  let descriptionLabel = UILabel().then {
+    $0.textColor = .systemGray
+    $0.font = .systemFont(ofSize: 15)
+    $0.text = "내 포인트"
+  }
+  
+  let pointLabel = UILabel().then {
+    $0.textColor = .black
+    $0.font = .systemFont(ofSize: 20, weight: .bold)
+    $0.text = "0 원"
+  }
+  
+  let withdrawButton = UIButton().then {
+    $0.setTitle("출금하기", for: .normal)
+    $0.setTitleColor(.white, for: .normal)
+    $0.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+    $0.backgroundColor = .tintColor
+    $0.layer.masksToBounds = true
+    $0.layer.cornerRadius = 5
+  }
+
+  let useButton = UIButton().then {
+    $0.setTitle("사용하기", for: .normal)
+    $0.setTitleColor(.white, for: .normal)
+    $0.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+    $0.backgroundColor = .tintColor
+    $0.layer.masksToBounds = true
+    $0.layer.cornerRadius = 5
+  }
+  
+  lazy var btnSV = UIStackView().then {
+    $0.axis = .horizontal
+    $0.distribution = .fillEqually
+    $0.spacing = 15
+  }
   
   var point: MyPoint = .default
   
+  // MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+  
+  // MARK: - Setup
+  override func setupLayouts() {
+
+    view.addSubview(stackView)
+    
+    let _ = [descriptionLabel, pointLabel, btnSV].map {
+      stackView.addArrangedSubview($0)
+    }
+    let _ = [withdrawButton, useButton].map {
+      btnSV.addArrangedSubview($0)
+    }
+  }
+  
+  override func setupConstraints() {
+    stackView.snp.makeConstraints {
+      $0.left.right.equalToSuperview().inset(10)
+      $0.top.equalToSuperview().inset(40)
+    }
+    
+    btnSV.snp.makeConstraints {
+      $0.height.equalTo(50)
+    }
+    
+  }
+  override func setupStyles() {
+    
   }
 }
 
@@ -25,3 +98,7 @@ struct MyPointViewControllerPreView:
   }
 }
 #endif
+
+
+
+
